0720c33ef64a77b75e61f2019074da09f984c876
Ivan Ferreira <ivanjuniomf@gmail.com>
2024-10-18 10:38:04 -0300

    Validação de campos

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://example.cypress.io',
+    baseUrl: 'http://localhost:3000/',
     viewportHeight: 1080,
     viewportWidth: 1920,
     retries: {
