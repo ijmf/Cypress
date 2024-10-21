e66fb2d730e532e306f877596759cc7b4160e29d
Ivan Ferreira <ivanjuniomf@gmail.com>
2024-10-21 16:50:55 -0300

    Revert "Conceitos: Sessions, Variaveis de ambiente, boas práticas Javascript."
    This reverts commit 2f96c3d24357bb1586d4cf8436a37f090c4335d5.

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // URL padrão
-    env: {
-      apiBaseUrl: 'http://localhost:5000' // Nova variável de ambiente
-    },
+    baseUrl: 'http://localhost:3000/',
     viewportHeight: 1080,
     viewportWidth: 1920,
     retries: {
--- cypress/e2e/api/naoLogado.cy.js
+++ cypress/e2e/api/naoLogado.cy.js
@@ -2,9 +2,8 @@
 
 describe('API - Profile', () => {
 
-    context('Seleciona todos os perfis cadastrados no banco', () => {
-
-        it('Todos os perfis', () => {
+    context('valida a API de perfis', () => {
+        it('todos os perfis', () => {
             cy.request({
                 url: 'api/profile',
                 method: 'GET'
@@ -15,40 +14,7 @@ describe('API - Profile', () => {
                 expect(body[0].user.name, 'Nome do Usuário').to.eq('Igor Diga')
                 expect(body[0].skills, 'Habilidades').to.have.lengthOf(3)
                 expect(body[0].date, 'Campo de Data').to.not.be.null
-
             });
         });
     });
-
-    context('Seleciona o perfil do usuário logado com base no ID informado no parâmetro de path', () => {
-        it('Perfil identificado pelo ID', () => {
-            const userId = '67101d29e9b46e8b38acacb1'; // Substitua pelo ID do usuário desejado
-
-            cy.request({
-                url: `api/profile/user/${userId}`, // Incluindo o ID na URL
-                method: 'GET'
-            }).then(({ status, duration, body }) => {
-                expect(status, 'Status Code').to.eq(200); // Verifica se o status é 200
-                expect(body.user, 'ID deve ser igual').to.have.property('_id', userId); // Verifica se o ID no corpo da resposta é o mesmo que o ID pesquisado
-                expect(duration, 'Duração').to.be.lessThan(1000)
-                // Adicione mais verificações conforme necessário
-            });
-        });
-        it('Perfil não identificado pelo ID', () => {
-
-            const invalidUserId = '999'; // ID que você sabe que não existe
-
-            // Agora, faça a requisição com um ID inválido
-            cy.request({
-                url: `api/profile/user/${invalidUserId}`,
-                method: 'GET',
-                failOnStatusCode: false // Permitir que o teste continue mesmo em erro
-            }).then(({ status, body }) => {
-                expect(status, 'Status Code').to.eq(404); // Espera que o status seja 404 para um ID inválido
-                expect(body.errors[0].msg, 'Mensagem de erro').to.eq('Perfil não encontrado')
-            });
-        });
-
-    });
-
 }); 
