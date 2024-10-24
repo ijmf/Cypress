5f7ffeffb8239a451a7b4b710769a63abf6ab566
Ivan Ferreira <ivanjuniomf@gmail.com>
2024-10-24 19:26:00 -0300

    Atualização de Código

--- cypress/e2e/UI/conexaoQA_home.cy.js
+++ cypress/e2e/UI/conexaoQA_home.cy.js
@@ -4,34 +4,52 @@ describe('Página inicial', () => {
         cy.visit('/') // Visita a página inicial
     })
 
-    it('Válida o título da página inicial com GET', () => {
-        cy.get('[data-test=landing-title]') // pega a seleção do elemento
+    it('Válida o título central da página inicial ', () => {
+        cy.getElement('landing-title')
             .should('have.text', 'Conectando QAs ...') // Substitua 'Título Esperado' pelo texto que você espera
             .and('have.class', 'x-large') // Verifica se o título está visível
 
     });
 
-    it('Seleciona um elemento com o CONTAINS', () => {
-        cy.contains('h1', 'QAs')
-            .should('have.text', 'Conectando QAs ...')
-    })
+    it('Válida o título da página inicial', () => {
 
-    it('Valida propriedade CSS', () => {
-        cy.get('[data-test=landing-register]')
+        cy.getElement('navbar-conexaoQA') // pega a seleção do elemento
+            .should('have.text', ' ConexãoQA') // Substitua 'Título Esperado' pelo texto que você espera
+        //.and('have.class', 'x-large') // Verifica se o título está visível
+
+    });
+
+    it('Válida o título secundário da página inicial', () => {
+        cy.getElement('landing-subtitle') // pega a seleção do elemento
+            .should('have.text', 'Crie um perfil/portfolio, compartilhe posts e obtenha ajuda da comunidade de QA') // Substitua 'Título Esperado' pelo texto que você espera
+        //.and('have.class', 'x-large') // Verifica se o título está visível
+
+    });
+
+    it('Valida Botões e Propriedade CSS', () => {
+        cy.getElement('landing-register')
+            .should('have.text', 'Cadastrar')
             .should('have.css', 'color', 'rgb(255, 255, 255)')
-        cy.get('[data-test=landing-login]')
+        cy.getElement('landing-login')
+            .should('have.text', 'Login')
             .should('have.css', 'color', 'rgb(51, 51, 51)')
     })
 
-    it('Seleciona elemento utilizando o Filter ou Eq', () => {
-        //Filtrar utilizando o comando filter
-        cy.get('a')
-            .filter('.btn-primary')
-            .should('have.text', 'Cadastrar')
+    // it('Seleciona elemento utilizando o Filter ou Eq', () => {
+    //     //Filtrar utilizando o comando filter
+    //     cy.get('a')
+    //         .filter('.btn-primary')
+    //         .should('have.text', 'Cadastrar')
 
-        //Filtrar utilizando o comando eq
-        cy.get('a')
-            .eq(6)
-            .should('have.text', 'Login')
-    })
+    //     //Filtrar utilizando o comando eq
+    //     cy.get('a')
+    //         .eq(6)
+    //         .should('have.text', 'Login')
+    // })
+
+    // it('Seleciona um elemento com o CONTAINS', () => {
+    //     cy.contains('h1', 'QAs')
+    //         .should('have.text', 'Conectando QAs ...')
+    // })
+
 });
